6a644f9da2c6ea45b470c017aa43ae9052344caf
Samuelson Brito <samuelsonma@gmail.com>
2019-10-20 02:51:46 -0400

    consumo de api

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,79 @@
 <template>
   <div id="app">
     <div class="header">
-      <header>
+      <header class="text-center">
         <h3>Nome da aplicação</h3>
+        <select name id v-model="layerSelect" @change="selectLayer($event)" class="form-control">
+          <option selected value="0">Selecione uma camada para visualização</option>
+          <option value="1">Temperatura da superfície da terra (LST)</option>
+          <option value="2">Índice de vegetação</option>
+        </select>
       </header>
     </div>
 
-    <div class="container">
+    <div class="container-nasa">
       <div class="globo">
         <canvas id="canvasOne" width="600" height="800">Your browser does not support HTML5 Canvas.</canvas>
       </div>
 
-      <div class="card" v-show="this.clickMap">
-        <div class="card-titulo">
-          <h1>Titulo</h1>
-        </div>
-        <div class="info">
-          <label>Temperatura atual</label>
-          <h3>
-            teste
+      <div class="card-nasa" v-show="this.clickMap">
+        <div v-for="infor in informations" :key="infor.id">
+          <div class="card-titulo">
+            <h1>{{infor.location_name}}</h1>
+          </div>
+          <div class="info">
+            <label>Temperatura atual</label>
+            <h3>
+              {{infor.temperature}}
+              <hr />
+            </h3>
+          </div>
+
+          <div class="info">
+            <label>Mudanças na temperatura</label>
+            <div v-for="inforTemp in infor.temperature_historic" :key="inforTemp.id">
+              <h3>{{inforTemp.value}}° em {{inforTemp.data_create}}</h3>
+            </div>
             <hr />
-          </h3>
-        </div>
+          </div>
 
-        <div class="info">
-          <label>Mudanças na temperatura</label>
-          <h3>30° em 2010</h3>
-          <h3>30° em 2010</h3>
-          <h3>30° em 2010</h3>
-          <h3>30° em 2010</h3>
-          <hr />
-        </div>
+          <div class="info">
+            <label>Taxa de vegetação atual</label>
+            <h3>
+              {{infor.vegetation_rate}}%
+              <hr />
+            </h3>
+          </div>
 
-        <div class="info">
-          <label>Taxa de vegetação atual</label>
-          <h3>
-            teste
+          <div class="info">
+            <label>Mudanças na taxa da vegetação</label>
+            <div v-for="inforVeg in infor.vegetation_rate_historic" :key="inforVeg.id">
+              <h3>{{inforVeg.value}}% em {{inforVeg.data_create}}</h3>
+            </div>
             <hr />
-          </h3>
-        </div>
+          </div>
 
-        <div class="info">
-          <label>Mudanças na taxa da vegetação</label>
-          <h3>30% em 2010</h3>
-          <h3>30% em 2010</h3>
-          <h3>30% em 2010</h3>
-          <h3>30% em 2010</h3>
-          <hr />
-        </div>
+          <div class="info">
+            <label>Taxa de incidência populacional atual</label>
+            <h3>
+              {{infor.population_incident}}%
+              <hr />
+            </h3>
+          </div>
 
-        <div class="info">
-          <label>Taxa de incidência populacional atual</label>
-          <h3>
-            teste
+          <div class="info">
+            <label>Mudanças na taxa de incidência populacional</label>
+            <div v-for="inforInc in infor.population_incident_historic" :key="inforInc.id">
+              <h3>{{inforInc.value}}% em {{inforInc.data_create}}</h3>
+            </div>
             <hr />
-          </h3>
-        </div>
+          </div>
 
-        <div class="info">
-          <label>Mudanças na taxa de incidência populacional</label>
-          <h3>30% em 2010</h3>
-          <h3>30% em 2010</h3>
-          <h3>30% em 2010</h3>
-          <h3>30% em 2010</h3>
-          <hr />
+          <h3>Ocorrencias</h3>
+          <ul>
+            <li>EX1</li>
+          </ul>
         </div>
-
-        <h3>Ocorrencias</h3>
-        <ul>
-          <li>EX1</li>
-        </ul>
       </div>
     </div>
   </div>
@@ -77,6 +81,8 @@
 
 <script>
 import worldwind from "@nasaworldwind/worldwind";
+import $ from "jquery";
+import axios from "axios";
 
 export default {
   name: "app",
@@ -84,11 +90,17 @@ export default {
   data() {
     return {
       clickMap: false,
-      wwd: null
+      wwd: null,
+      layerSelect: null,
+      informations: null
     };
   },
 
-  created() {},
+  beforeCreate() {},
+
+  created() {
+    this.getApi();
+  },
 
   mounted() {
     this.wwd = new worldwind.WorldWindow("canvasOne");
@@ -112,10 +124,63 @@ export default {
       this.wwd.addLayer(layers[l].layer);
     }
 
-    this.clickMarker(this.wwd);
+    this.markerDefault(this.wwd)
+
+    this.clickMarker(this.wwd)
   },
 
   methods: {
+    getApi() {
+      axios
+        .get(`https://api.jsonbin.io/b/5dabe6f645e66e5713fb45a8`)
+        .then(response => {
+          this.informations = response.data;
+          console.log(JSON.stringify(this.informations));
+        });
+    },
+
+    markerDefault(wwd) {
+      var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
+      wwd.addLayer(placemarkLayer);
+      var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+
+      placemarkAttributes.imageOffset = new WorldWind.Offset(
+        WorldWind.OFFSET_FRACTION,
+        0.3,
+        WorldWind.OFFSET_FRACTION,
+        0.0
+      );
+
+      placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
+      placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+        WorldWind.OFFSET_FRACTION,
+        0.5,
+        WorldWind.OFFSET_FRACTION,
+        1.0
+      );
+
+
+      placemarkAttributes.imageSource = require("./assets/images/marker.png");
+alert(WorldWind.configuration.baseUrl)
+      var position = new WorldWind.Position(-3.092002, -59.945043000000005, 100.0);
+      var placemark = new WorldWind.Placemark(
+        position,
+        false,
+        placemarkAttributes
+      );
+
+      placemark.label =
+        "Amazonas\n" +
+        "Lat " +
+        placemark.position.latitude.toPrecision(4).toString() +
+        "\n" +
+        "Lon " +
+        placemark.position.longitude.toPrecision(5).toString();
+      placemark.alwaysOnTop = true;
+
+      placemarkLayer.addRenderable(placemark);
+    },
+
     clickMarker(wwd) {
       var vm = this;
 
@@ -131,57 +196,13 @@ export default {
           if (pickList.objects.length == 1 && pickList.objects[0].isTerrain) {
             var mapPosition = pickList.objects[0].position;
 
-            var placemarkLayer = new worldwind.RenderableLayer("Placemark");
-            wwd.addLayer(placemarkLayer);
-
-            var placemarkAttributes = new worldwind.PlacemarkAttributes(null);
-
-            placemarkAttributes.imageOffset = new worldwind.Offset(
-              WorldWind.OFFSET_FRACTION,
-              0.3,
-              WorldWind.OFFSET_FRACTION,
-              0.0
-            );
-
-            placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
-            placemarkAttributes.labelAttributes.offset = new worldwind.Offset(
-              WorldWind.OFFSET_FRACTION,
-              0.5,
-              WorldWind.OFFSET_FRACTION,
-              1.0
-            );
-
-            placemarkAttributes.imageSource =
-              WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";
-
-            var position = new worldwind.Position(
-              mapPosition.latitude,
-              mapPosition.longitude,
-              100.0
-            );
-            var placemark = new worldwind.Placemark(
-              position,
-              false,
-              placemarkAttributes
-            );
-
-            placemark.label =
-              "+\n" +
-              "Lat " +
-              placemark.position.latitude.toPrecision(4).toString() +
-              "\n" +
-              "Lon " +
-              placemark.position.longitude.toPrecision(5).toString();
-            placemark.alwaysOnTop = true;
-
-            placemarkLayer.addRenderable(placemark);
-
             wwd.goTo(
               new WorldWind.Location(
                 mapPosition.latitude,
                 mapPosition.longitude
               )
-            );
+            )
+
             vm.buildMarker();
           }
         });
@@ -190,6 +211,45 @@ export default {
 
     buildMarker() {
       this.clickMap = !this.clickMap;
+    },
+
+    selectLayer(event) {
+      var vm = this;
+      var serviceAddress =
+        "https://neo.sci.gsfc.nasa.gov/wms/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0";
+      var layer = this.layerSelect;
+
+      if (layer == "1") {
+        var layerName = "MOD_LSTD_M";
+
+        $.get(serviceAddress)
+          .done(function(xmlDom) {
+            var wms = new worldwind.WmsCapabilities(xmlDom);
+            var wmsLayerCapabilities = wms.getNamedLayer(layerName);
+            var wmsConfig = WorldWind.WmsLayer.formLayerConfiguration(
+              wmsLayerCapabilities
+            );
+            var wmsLayer = new worldwind.WmsLayer(wmsConfig);
+            vm.wwd.addLayer(wmsLayer);
+          })
+          .fail(function() {});
+      } else if (layer == "2") {
+        var layerName = "MOD_NDVI_M";
+
+        $.get(serviceAddress)
+          .done(function(xmlDom) {
+            var wms = new WorldWind.WmsCapabilities(xmlDom);
+            var wmsLayerCapabilities = wms.getNamedLayer(layerName);
+            var wmsConfig = WorldWind.WmsLayer.formLayerConfiguration(
+              wmsLayerCapabilities
+            );
+            var wmsLayer = new WorldWind.WmsLayer(wmsConfig);
+            vm.wwd.addLayer(wmsLayer);
+          })
+          .fail(function() {});
+      } else {
+        vm.wwd.addLayer(null);
+      }
     }
   }
 };
@@ -210,7 +270,7 @@ export default {
   margin: 0;
 }
 
-.container {
+.container-nasa {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -230,7 +290,7 @@ export default {
   max-height: calc(100vh - 30px);
 }
 
-.card {
+.card-nasa {
   max-height: calc(100vh - 30px);
   width: calc(100vw - 70%);
   background-color: transparent;
@@ -241,17 +301,17 @@ export default {
   border-color: #a2a2a2f8;
 }
 
-.card * {
+.card-nasa * {
   color: #ffffff;
   padding: 10px 5px;
 }
 
-.card hr {
+.card-nasa hr {
   padding: 0;
   margin: 3px 0;
 }
 
-.card .card-titulo {
+.card-nasa .card-titulo {
   text-align: center;
   background-color: #7f66d4;
 }
