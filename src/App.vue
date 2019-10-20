ce876bc25376287b72394ef042cbadc6f1fe0cee
Pietro Souza <pietro.zanelli.marinho@gmail.com>
2019-10-19 23:19:30 -0400

    settings

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,82 @@
 <template>
   <div id="app">
-    <div class="globo">
-      <canvas id="canvasOne" width="600" height="800">Your browser does not support HTML5 Canvas.</canvas>
+    <div class="header">
+      <header>
+        <h3>Nome da aplicação</h3>
+      </header>
     </div>
 
-    <div class="card" v-show="this.clickMap">teste</div>
+    <div class="container">
+      <div class="globo">
+        <canvas id="canvasOne" width="600" height="800">Your browser does not support HTML5 Canvas.</canvas>
+      </div>
+
+      <div class="card" v-show="this.clickMap">
+        <div class="card-titulo">
+          <h1>Titulo</h1>
+        </div>
+        <div class="info">
+          <label>Temperatura atual</label>
+          <h3>
+            teste
+            <hr />
+          </h3>
+        </div>
+
+        <div class="info">
+          <label>Mudanças na temperatura</label>
+          <h3>30° em 2010</h3>
+          <h3>30° em 2010</h3>
+          <h3>30° em 2010</h3>
+          <h3>30° em 2010</h3>
+          <hr />
+        </div>
+
+        <div class="info">
+          <label>Taxa de vegetação atual</label>
+          <h3>
+            teste
+            <hr />
+          </h3>
+        </div>
+
+        <div class="info">
+          <label>Mudanças na taxa da vegetação</label>
+          <h3>30% em 2010</h3>
+          <h3>30% em 2010</h3>
+          <h3>30% em 2010</h3>
+          <h3>30% em 2010</h3>
+          <hr />
+        </div>
+
+        <div class="info">
+          <label>Taxa de incidência populacional atual</label>
+          <h3>
+            teste
+            <hr />
+          </h3>
+        </div>
+
+        <div class="info">
+          <label>Mudanças na taxa de incidência populacional</label>
+          <h3>30% em 2010</h3>
+          <h3>30% em 2010</h3>
+          <h3>30% em 2010</h3>
+          <h3>30% em 2010</h3>
+          <hr />
+        </div>
+
+        <h3>Ocorrencias</h3>
+        <ul>
+          <li>EX1</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import worldwind from "@nasaworldwind/worldwind"
+import worldwind from "@nasaworldwind/worldwind";
 
 export default {
   name: "app",
@@ -21,9 +88,7 @@ export default {
     };
   },
 
-  created() {
-    
-  },
+  created() {},
 
   mounted() {
     this.wwd = new worldwind.WorldWindow("canvasOne");
@@ -52,7 +117,6 @@ export default {
 
   methods: {
     clickMarker(wwd) {
-
       var vm = this;
 
       wwd.addEventListener("mousemove", function(event) {
@@ -93,7 +157,7 @@ export default {
             var position = new worldwind.Position(
               mapPosition.latitude,
               mapPosition.longitude,
-              50.0
+              100.0
             );
             var placemark = new worldwind.Placemark(
               position,
@@ -112,16 +176,20 @@ export default {
 
             placemarkLayer.addRenderable(placemark);
 
-            wwd.goTo(new WorldWind.Location(mapPosition.latitude, mapPosition.longitude));
+            wwd.goTo(
+              new WorldWind.Location(
+                mapPosition.latitude,
+                mapPosition.longitude
+              )
+            );
             vm.buildMarker();
-
           }
         });
       });
     },
 
     buildMarker() {
-      this.clickMap = !this.clickMap    
+      this.clickMap = !this.clickMap;
     }
   }
 };
@@ -129,10 +197,23 @@ export default {
 
 <style>
 #app {
+  background-color: #000;
+  max-height: 100vh;
+}
+
+.header {
+  background-color: #7f66d4;
+  color: #ffffff;
+}
+
+.header * {
+  margin: 0;
+}
+
+.container {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   display: flex;
   align-items: center;
@@ -143,9 +224,53 @@ export default {
   width: calc(100vw - 30%);
 }
 
+#canvasOne {
+  max-width: calc(100vw - 45%);
+  width: calc(100vw - 46%);
+  max-height: calc(100vh - 30px);
+}
+
 .card {
-  background-color: #000;
-  height: 100vh;
+  max-height: calc(100vh - 30px);
   width: calc(100vw - 70%);
+  background-color: transparent;
+  margin-right: 10px;
+  overflow-y: auto;
+  border-style: solid;
+  border-width: 3px;
+  border-color: #a2a2a2f8;
+}
+
+.card * {
+  color: #ffffff;
+  padding: 10px 5px;
+}
+
+.card hr {
+  padding: 0;
+  margin: 3px 0;
+}
+
+.card .card-titulo {
+  text-align: center;
+  background-color: #7f66d4;
+}
+
+.card-titulo * {
+  margin: 0;
+}
+
+h3 {
+  margin: 0;
+  padding: 0px 8px;
+}
+
+ul {
+  margin: 0;
+  padding: 0 5px !important;
+}
+
+ul *li {
+  padding: 0 5px;
 }
 </style>
